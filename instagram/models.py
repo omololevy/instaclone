672e804ceb3cce77c725013650bb8f62dae82486
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-12-09 02:14:00 +0300

    Change the default image from storage to cloudinary

--- instagram/models.py
+++ instagram/models.py
@@ -9,7 +9,7 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,primary_key=True)
-    profile_picture = CloudinaryField('pictures/',default='pictures/user.png')
+    profile_picture = CloudinaryField('pictures/',default='http://res.cloudinary.com/dim8pysls/image/upload/v1639001486/x3mgnqmbi73lten4ewzv.png')
     bio = models.TextField(max_length=500, default="My Bio", blank=True)
     name = models.CharField(max_length=250, blank=True)
   
