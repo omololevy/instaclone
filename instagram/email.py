9a3d1f21010bbf28e40befaf030f6a1d7b648cd2
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-12-08 19:38:10 +0300

    change the email sender in the email python file

--- instagram/email.py
+++ instagram/email.py
@@ -4,7 +4,7 @@ from django.template.loader import render_to_string
 def send_welcome_email(name,receiver):
    
     subject = 'Welcome to the Instagram'
-    sender = 'COTECH'
+    sender = 'cotechlevy@gmail.com'
 
     #passing in the context vairables
     text_content = render_to_string('email/instaemail.txt',{"name": name})
